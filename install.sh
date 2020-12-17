ec5d5658146efd0d117a440a8e74ec89a5c0747f
David Li <davidli012345@gmail.com>
2020-12-17 20:26:03 +0000

    updating requirements

--- install.sh
+++ install.sh
@@ -4,6 +4,5 @@ conda create -n myenv python=3.6.9
 conda init bash
 bash
 conda activate myenv
-conda install -c conda-forge voila
-pip install numpy
+pip install numpy pandas
 pip install -r requirements.txt
